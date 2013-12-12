33b1c59cd7c506f3d2cabcbb8b3a839076592485
Kyle Isom <kyle@tyrfingr.is>
2013-12-11 20:23:05 -0700

    Add PRNG code.
    The full PRNG implementation is complete; testing is in progress.

---
+++ fortuna/prng.go
@@ -0,0 +1,214 @@
+package fortuna
+
+import (
+	"crypto/sha256"
+	"errors"
+	"hash"
+	"io/ioutil"
+	"sync"
+	"time"
+)
+
+// MinPoolSize stores the number of bytes that will trigger a reseed.
+// The ReseedDelay prevents reseed events from occuring too quickly.
+var (
+	MinPoolSize int64 = 32
+	ReseedDelay       = 100 * time.Millisecond
+)
+
+// MaxEventSize is the limit to the amount of data that can be sent
+// in an event.
+const MaxEventSize = 32
+
+// PoolSize contains the number of pools used by the PRNG.
+const PoolSize = 32
+
+// SeedFileLength is the number of bytes that should be present in
+// the seed file.
+const SeedFileLength = 64
+
+var (
+	ErrNotSeeded    = errors.New("fortuna: PRNG not seeded yet")
+	ErrInvalidEvent = errors.New("fortuna: invalid random event")
+	ErrInvalidSeed  = errors.New("fortuna: invalid seed")
+)
+
+type pool struct {
+	hash    []byte
+	written int64
+	sync.Mutex
+}
+
+type reseedTime struct {
+	time.Time
+	sync.Mutex
+}
+
+type Fortuna struct {
+	initialised bool
+	pools       *[32]*pool
+	counter     uint32
+	g           *Generator
+	h           func() hash.Hash
+	lastReseed  *reseedTime
+}
+
+// Initialised returns true if the rng is initialised.
+func (rng *Fortuna) Initialised() bool {
+	if rng == nil {
+		return false
+	}
+	return rng.initialised
+}
+
+// New sets up a new Fortuna PRNG; it is required for ensuring that
+// the PRNG is properly initialised.
+func New(h func() hash.Hash) *Fortuna {
+	if h == nil {
+		h = sha256.New
+	}
+	rng := &Fortuna{
+		pools:      new([32]*pool),
+		g:          NewGenerator(),
+		h:          h,
+		lastReseed: &reseedTime{},
+	}
+
+	for i := range rng.pools {
+		rng.pools[i] = &pool{
+			hash: []byte{},
+		}
+	}
+
+	rng.initialised = true
+	return rng
+}
+
+func (rng *Fortuna) mustReseed() bool {
+	poolReseed := rng.pools[0].written >= MinPoolSize
+	rng.lastReseed.Lock()
+	reseed := rng.lastReseed.Time.Add(ReseedDelay)
+	rng.lastReseed.Unlock()
+	return poolReseed && time.Now().After(reseed)
+}
+
+func (rng *Fortuna) reseed() {
+	rng.counter++
+	s := []byte{}
+
+	for i := 0; i < len(rng.pools); i++ {
+		if ((1 << uint32(i)) | rng.counter) != 0 {
+			rng.pools[i].Lock()
+			h := rng.h()
+			h.Write(rng.pools[i].hash)
+			s = append(s, h.Sum(nil)...)
+			rng.pools[i].hash = []byte{}
+			rng.pools[i].Unlock()
+		}
+	}
+	rng.g.Write(s)
+	rng.lastReseed.Lock()
+	rng.lastReseed.Time = time.Now()
+	rng.lastReseed.Unlock()
+}
+
+func (rng *Fortuna) Read(p []byte) (int, error) {
+	if rng.mustReseed() {
+		rng.reseed()
+	}
+
+	if rng.counter == 0 {
+		return 0, ErrNotSeeded
+	}
+
+	if p == nil {
+		return 0, nil
+	}
+
+	return rng.g.Read(p)
+}
+
+// AddRandomEvent should be called by sources to add random events
+// to the PRNG; it takes a source identifier, a pool number, and a
+// random event. Sources should cycle through pools, evenly
+// distributing events over the entire set of pools; the Fortuna
+// designers specify that this should be done "in a round-robin
+// fashion." The choice of a source identifier is up to the host
+// application.
+func (rng *Fortuna) AddRandomEvent(s byte, i int, e []byte) error {
+	if e == nil || len(e) == 0 || len(e) > MaxEventSize {
+		return ErrInvalidEvent
+	}
+
+	if i < 0 || i > len(rng.pools) {
+		return ErrInvalidEvent
+	}
+
+	rng.pools[i].hash = append(rng.pools[i].hash, s)
+	rng.pools[i].hash = append(rng.pools[i].hash, byte(len(e)))
+	rng.pools[i].hash = append(rng.pools[i].hash, e...)
+	rng.pools[i].written += int64(len(e) + 2)
+	return nil
+}
+
+// Seed dumps a byte slice containing a seed that may be used to
+// restore the PRNG's state.
+func (rng *Fortuna) Seed() ([]byte, error) {
+	var p = make([]byte, SeedFileLength)
+	n, err := rng.Read(p)
+	if err != nil {
+		return nil, err
+	} else if n != SeedFileLength {
+		return nil, ErrInvalidSeed
+	}
+	return p, nil
+}
+
+// WriteSeed writes a seed to a file; this should be used for
+// restoring the PRNG state later.
+func (rng *Fortuna) WriteSeed(filename string) error {
+	seed, err := rng.Seed()
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(filename, seed, 0600)
+}
+
+// UpdateSeed reads a seed from a file and updates the seed file
+// with new random data.
+func (rng *Fortuna) UpdateSeed(filename string) error {
+	seed, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	} else if len(seed) != SeedFileLength {
+		return ErrInvalidSeed
+	}
+
+	rng.g.Write(seed)
+	return rng.WriteSeed(filename)
+}
+
+// ReadSeed reseeds the PRNG with a seed that is expected to have
+// been read from a seed file.
+func (rng *Fortuna) ReadSeed(p []byte) error {
+	if len(p) != SeedFileLength {
+		return ErrInvalidSeed
+	}
+	rng.g.Write(p)
+	return nil
+}
+
+// FromSeed creates a new PRNG instance from the seed file. This
+// can be used to start an RNG on start up.
+func FromSeed(filename string, h func() hash.Hash) (*Fortuna, error) {
+	seed, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	} else if len(seed) != SeedFileLength {
+		return nil, ErrInvalidSeed
+	}
+
+	rng := New(h)
+	rng.g.Write(seed)
+	return rng, nil
+}
---
+++ fortuna/prng_test.go
@@ -0,0 +1,92 @@
+package fortuna
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"testing"
+)
+
+func TestNilRNG(t *testing.T) {
+	var rng *Fortuna
+	if rng.Initialised() {
+		fmt.Fprintf(os.Stderr, "fortuna: PRNG should not have reported it was initialised\n")
+		t.FailNow()
+	}
+}
+
+func TestNotSeeded(t *testing.T) {
+	var p []byte
+	rng := New(nil)
+	if _, err := rng.Read(p); err == nil {
+		fmt.Fprintf(os.Stderr, "fortuna: PRNG should report it is not seeded")
+		t.FailNow()
+	}
+}
+
+func TestPRNGEmptyRead(t *testing.T) {
+	var p []byte
+	rng := New(nil)
+	rng.reseed()
+
+	n, err := rng.Read(p)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		t.FailNow()
+	} else if n != 0 {
+		fmt.Fprintf(os.Stderr, "fortuna: no data should have been read\n")
+		t.FailNow()
+	}
+}
+
+func TestInvalidEvents(t *testing.T) {
+	var p = make([]byte, 1)
+	rng := New(nil)
+	err := rng.AddRandomEvent(0, 33, p)
+	if err != ErrInvalidEvent {
+		fmt.Fprintf(os.Stderr, "fortuna: random event should be invalid\n")
+		t.FailNow()
+	}
+
+	err = rng.AddRandomEvent(0, -1, p)
+	if err != ErrInvalidEvent {
+		fmt.Fprintf(os.Stderr, "fortuna: random event should be invalid\n")
+		t.FailNow()
+	}
+
+	p = nil
+	err = rng.AddRandomEvent(0, 0, p)
+	if err != ErrInvalidEvent {
+		fmt.Fprintf(os.Stderr, "fortuna: random event should be invalid\n")
+		t.FailNow()
+	}
+
+	p = make([]byte, MaxEventSize+1)
+	err = rng.AddRandomEvent(0, 0, p)
+	if err != ErrInvalidEvent {
+		fmt.Fprintf(os.Stderr, "fortuna: random event should be invalid\n")
+		t.FailNow()
+	}
+}
+
+func TestSeed(t *testing.T) {
+	rng := New(nil)
+	sw := NewSourceWriter(rng, 0)
+
+	f, err := os.Open("/dev/zero")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		t.FailNow()
+	}
+
+	io.CopyN(sw, f, 4096)
+	seed, err := rng.Seed()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		t.FailNow()
+	} else if len(seed) != SeedFileLength {
+		fmt.Fprintf(os.Stderr, "fortuna: bad seed file length\n")
+		t.FailNow()
+	}
+
+}
